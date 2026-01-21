483b32d48042cc7a478f4ed6d92214c7ddc0bbf5
BillyMwangiDev <billymwangi200@gmail.com>
2026-01-21 11:26:16 +0300

    calculator

--- Python_data_types/calculator.py
+++ Python_data_types/calculator.py
@@ -1,5 +1,31 @@
 print(2+2)
+
+#Exponential
 print(2 ** 3)
 print(2 ** 3.)
 print(2. ** 3)
 print(2. ** 3.)
+
+#Multiplication
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+
+#Division
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+#Integer division(rounded off )
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+print(6 // 4)
+print(6. // 4)
+
+#Remainder(modulo)
+print(14 % 4)
